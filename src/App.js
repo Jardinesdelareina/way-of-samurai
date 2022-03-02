0663c0745fcdf72f93cce0be3b8501f3e048ff5f
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-03-03 00:29:14 +0300

    deploy 3.0

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ let AppContainer = compose(connect(mapStateToProps, { initApp }))(App);
 
 const SocialNetworkApp = (props) => {
   return (
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <AppContainer />
       </Provider>
